a7dfc78505b636e1dadf4581eab0c362cef292cc
JimenaAlexandraRuizCerna <jruiz@hiper.com.pe>
2021-06-12 18:13:03 -0500

    fix war

--- src/main/java/pe/edu/upc/CertificationAppApplication.java
+++ src/main/java/pe/edu/upc/CertificationAppApplication.java
@@ -1,13 +1,19 @@
 package pe.edu.upc;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CertificationAppApplication {
 
-	public static void main(String[] args) {
+    @PostConstruct
+    public void init(){
 		System.setProperty("jasypt.encryptor.password", "tdp");
+    }
+	
+	public static void main(String[] args) {
 		SpringApplication.run(CertificationAppApplication.class, args);
 	}
 }
